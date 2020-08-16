7e143069f73deb9c038476380982a64a292ecba2
JaysusM <jaysusm@github.com>
2020-08-16 21:00:34 +0200

    Add project structure

---
+++ src/Components/Demo/Demo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Button, { ButtonType } from '../Button/Button';
+import "./Demo.scss";
+
+export const Demo = () => {
+  return (
+    <>
+      Hello World
+    </>
+  );
+}
+
+export default Demo;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'www'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader',
+            ],
+          },
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+          },
+          {
+              test: /\.jsx?$/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/react', '@babel/env']
+                  }
+              },
+              exclude: /node_modules/
+          }
+        ],
+      },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
+    devServer: {
+        port: process.env.PORT || 8080,
+        contentBase: './src',
+        historyApiFallback: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+      filename: 'src/index.html'
+    })]
+};
