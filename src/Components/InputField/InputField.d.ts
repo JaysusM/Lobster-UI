7d750d78d041bf3d937956b57f0e2171d3bddb8f
JaysusM <jmartinruiz@uma.es>
2021-02-27 13:02:58 +0100

    Fix several bugs with TextField

--- src/Components/InputField/InputField.d.ts
+++ src/Components/InputField/InputField.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import "./inputfield.scss";
+import "./InputField.scss";
 import { DOMAttributes } from 'react';
 export declare type InputFieldType = "password" | "date" | "text";
 export interface InputFieldProps {
